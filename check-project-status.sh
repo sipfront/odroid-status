58fb5bfd5e7aaae99c62666698bf0254c26aa858
Andreas Granig <andreas@granig.com>
2024-01-20 15:44:45 +0100

    Add example to check all last runs of tests in project
    If any of the tests in the project are running, or if
    any of them have a failed last run, trigger the output.

--- check-project-status.sh
+++ check-project-status.sh
@@ -2,14 +2,14 @@
 
 pin=481 # GPIOX.5 / pin 7 (4th on row 1)
 auth=$1
-test_id=$2
+project_id=$2
 env=${3:-prod}
 
 dir_path="/sys/class/gpio/gpio${pin}/direction"
 val_path="/sys/class/gpio/gpio${pin}/value"
 
-if [ -z "$auth" ] || [ -z "$test_id" ]; then
-    echo "Usage: $0 <user:pass> <testid>"
+if [ -z "$auth" ] || [ -z "$project_id" ]; then
+    echo "Usage: $- <user:pass> <projectid>"
     exit 1
 fi
 
@@ -22,7 +22,8 @@ else
     exit 1
 fi
 
-url="https://${base}/api/v2/tests/${test_id}/status/last"
+url="https://${base}/api/v2/projects/${project_id}/status/last"
+tmpfile="/tmp/out-$$.json"
 
 if [ -e "$dir_path" ]; then
    echo "gpio pin $pin already exported..."
@@ -35,9 +36,12 @@ echo 0 > "$val_path"
 
 while [ 1 ]; do
     interval=5;
-    status=$(curl --user "$auth" -H 'Accept: application/json' "$url" 2>/dev/null | tac | tac | jq -r .run.status)
-    if [ "$status" = "running" ]; then
-        echo "running..."
+    
+    curl --user "$auth" -H 'Accept: application/json' "$url" --output "$tmpfile" 2>/dev/null
+    has_running=$(cat "$tmpfile" | jq 'any(.tests[]; .run.status == "running")' 2>/dev/null)
+    has_failed=$(cat "$tmpfile" | jq 'any(.tests[]; .run.status == "failed")' 2>/dev/null)
+    if [ "$has_running" = "true" ]; then
+        echo "has running..."
         for i in $(seq 1 3); do
             echo 1 > "$val_path"
             sleep 0.2
@@ -45,15 +49,13 @@ while [ 1 ]; do
             sleep 0.8
         done
         interval=2
-    elif [ "$status" = "passed" ]; then
-        echo "passed..."
-        echo 0 > "$val_path"
-    elif [ "$status" = "failed" ]; then
-        echo "failed..."
+    elif [ "$has_failed" = "true" ]; then
+        echo "has failed..."
         echo 1 > "$val_path"
+    else
+        echo "no running or failed..."
+        echo 0 > "$val_path"
     fi
 
     sleep "$interval"
 done
-
-
